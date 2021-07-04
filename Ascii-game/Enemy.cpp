55b74407fc069e1487834e5a9dc982428b934fb7
SIJ82 <si-j@outlook.it>
2021-07-04 23:15:18 +0200

    da

--- Ascii-game/Enemy.cpp
+++ Ascii-game/Enemy.cpp
@@ -4,37 +4,7 @@
 
 using namespace std;
 
-void Enemy::GetPosition(int &posX, int &posY)
-{
-	posX=x;
-	posY=y;
-}
 
-void Enemy::Move(int posX, int posY)
-{
-	x=posX;
-	y=posY;
-}
-
-void Enemy::Tick()
-{
-
-}
-
-void Enemy::setDifficulty(int setDiff)
-{
-	diffuculty = setDiff;
-}
-
-void Enemy::setHealth(int setH)
-{
-	health = setH;
-}
-
-double Enemy::getTotalDamage()
-{
-	return diffuculty * damage;
-}
 
 
 
