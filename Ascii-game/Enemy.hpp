eaf78903486ed896dc18b051565d1a0ed0a35a44
SIJ82 <si-j@outlook.it>
2021-07-04 23:00:22 +0200

    As

--- Ascii-game/Enemy.hpp
+++ Ascii-game/Enemy.hpp
@@ -1,20 +1,11 @@
 class Enemy
 {
 	protected:
-		struct Nodo
-		{
-			Nodo *next;
-			Nodo *prev;
-			
-			int idEnemy;
-			int x=0, y=0;
-			char ascii = '@';
-			int health = 100;
-			int damage = 10;
-			double diffuculty = 1;
-		};
-		
-		Nodo *testa, *coda;
+		int x=0, y=0;
+		char ascii = '@';
+		int health = 100;
+		int damage = 10;
+		double diffuculty = 1;
 	
 	public:
 		void GetPosition(int &posX, int &posY);
